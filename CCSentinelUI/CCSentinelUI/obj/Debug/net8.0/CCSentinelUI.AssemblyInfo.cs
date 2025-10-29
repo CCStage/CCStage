9787f74b007318f21fc31592c707750d61a3259d
CCStage <stage@clipconsultants.nl>
2025-10-29 17:23:19 +0100

    scopes afgewerkt

--- CCSentinelUI/CCSentinelUI/obj/Debug/net8.0/CCSentinelUI.AssemblyInfo.cs
+++ CCSentinelUI/CCSentinelUI/obj/Debug/net8.0/CCSentinelUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CCSentinelUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4457df6fe58e8232aece91cfda18ad5d275d9258")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+857c03606e96963c9f30fc86c909f7fc21e041f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CCSentinelUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CCSentinelUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
