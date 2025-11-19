63e5d61c44aaeffb48afb892f539dcd3b45c0856
CCStage <stage@clipconsultants.nl>
2025-11-19 17:28:19 +0100

    End of the day push
    fixed create bug and also made user able to log in if they have admin accese

--- CCSentinelUI/CCSentinelUI/obj/Debug/net8.0/CCSentinelUI.AssemblyInfo.cs
+++ CCSentinelUI/CCSentinelUI/obj/Debug/net8.0/CCSentinelUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CCSentinelUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da7fe7206e13b7c47c8f1e949ccb479d8ba6df28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+837bb1bfcefb053c10d5223d5c3453577f22f2e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CCSentinelUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CCSentinelUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
