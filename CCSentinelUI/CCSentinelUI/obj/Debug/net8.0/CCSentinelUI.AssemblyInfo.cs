abd96e1c3666b09033b8b653e35f34eb4c28d03c
CCStage <stage@clipconsultants.nl>
2025-11-20 17:29:55 +0100

    end of the day push

--- CCSentinelUI/CCSentinelUI/obj/Debug/net8.0/CCSentinelUI.AssemblyInfo.cs
+++ CCSentinelUI/CCSentinelUI/obj/Debug/net8.0/CCSentinelUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CCSentinelUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+837bb1bfcefb053c10d5223d5c3453577f22f2e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63e5d61c44aaeffb48afb892f539dcd3b45c0856")]
 [assembly: System.Reflection.AssemblyProductAttribute("CCSentinelUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CCSentinelUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
