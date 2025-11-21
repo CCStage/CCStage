85bd640b0de5ec688c17563f164b0c4894f2289a
CCStage <stage@clipconsultants.nl>
2025-11-21 16:30:08 +0100

    end of the day push

--- CCSentinelUI/CCSentinelUI/Data/Contracts/SentinelUserRole.cs
+++ CCSentinelUI/CCSentinelUI/Data/Contracts/SentinelUserRole.cs
@@ -4,10 +4,9 @@ namespace CCSentinelUI.Data.Contracts
 {
     public class SentinelUserRole : IdentityUserRole<Guid>
     {
-        /// <summary>
-        /// Gets or sets the primary key for this user role.
-        /// </summary>
-        [PersonalData]
-        public virtual Guid Id { get; set; } = default!;
+        public Guid Id { get; set; }
+
+        public SentinelUser User { get; set; }
+        public SentinelRole Role { get; set; }
     }
 }
--- CCSentinelUI/CCSentinelUI/obj/Debug/net8.0/CCSentinelUI.AssemblyInfo.cs
+++ CCSentinelUI/CCSentinelUI/obj/Debug/net8.0/CCSentinelUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CCSentinelUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63e5d61c44aaeffb48afb892f539dcd3b45c0856")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abd96e1c3666b09033b8b653e35f34eb4c28d03c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CCSentinelUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CCSentinelUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
